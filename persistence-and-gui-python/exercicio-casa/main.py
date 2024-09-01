dd0593472c61d33044bc17a56d5392a923fb7478
João Laranjeira <jpclrocha@gmail.com>
2024-09-01 07:49:37 -0300

    Improved the exercise response

--- persistence-and-gui-python/exercicio-casa/main.py
+++ persistence-and-gui-python/exercicio-casa/main.py
@@ -24,29 +24,29 @@ def getNomePessoaMaisVelha():
                 if(idade > idadeRef):
                     idadeRef = idade
                     nomeMaisVelha = pessoa[0]
-        print(f"Telefone da pessoa mais nova: {nomeMaisVelha}")
+        print(f"Telefone da pessoa mais velha: {nomeMaisVelha}")
 
 def getMediaIdade():
     with open("ex-casa.txt", "r", encoding="utf-8") as arquivo:
         lista = arquivo.read().split("\n")
+        somaIdade = 0
+        for item in lista:
+            pessoa = item.split("|")
+            if(len(pessoa) > 1):
+                somaIdade += int(pessoa[1])
+        media = somaIdade / (len(lista) - 1)
+        print(f"Media de idade: {media}")
         print(lista)
-        # somaIdade = 0
-        # for item in lista:
-        #     pessoa = item.split("|")
-        #     if(len(pessoa) > 1):
-        #         somaIdade += int(pessoa[1])
-        # media = somaIdade / len(lista)
-        # print(f"Telefone da pessoa mais nova: {media}")
 
             
 
-def main():
+def adicionarPessoas():
     nome  = ""
     while True:
         nome = str(input("Digite o nome: "))
         
         if(nome == "0"):
-            print("Voce escolheu sair!")
+            print("Voce voltou ao menu principal!")
             break
         
         idade = int(input("Digite a idade: "))
@@ -57,8 +57,27 @@ def main():
         with open("ex-casa.txt", "a", encoding="utf-8") as arquivo:
             arquivo.write(f"{nome}|{idade}|{sexo}|{telefone}\n")
 
+def menu():
+    opcao = ""
+    while(opcao != "5"):
+        print("1 - Adicionar pessoas")
+        print("2 - Telefone da pessoa mais nova")
+        print("3 - Nome da pessoa mais velha")
+        print("4 - Media de idade")
+        print("5 - Sair")
+        opcao = str(input("Digite sua escolha: "))
 
-main()
-getTelefonePessoaMaisNova()
-getNomePessoaMaisVelha()
-getMediaIdade()
+        if(opcao == "1"):
+            adicionarPessoas()
+        elif(opcao == "2"):
+            getTelefonePessoaMaisNova()
+        elif(opcao == "3"):
+            getNomePessoaMaisVelha()
+        elif(opcao == "4"):
+            getMediaIdade()
+        elif(opcao == "5"):
+            print("Voce escolheu sair")
+        else:
+            print("Opcao invalida!")
+
+menu()
