fbc4f0850c38d69486ca914e58e3011a6e1d2950
João Laranjeira <jpclrocha@gmail.com>
2024-09-26 01:03:53 -0300

    Finished the exercise

--- persistence-and-gui-python/ex-gui-01/main.py
+++ persistence-and-gui-python/ex-gui-01/main.py
@@ -2,11 +2,14 @@ from tkinter import *
 from tkinter import messagebox
 
 FILENAME = "ex.txt"
+is_updating = False
+selected_index = None
+
 def carregar_items():
-    with open(FILENAME, "r", encoding="utf-8") as arquivo:
-        lista = arquivo.readlines()
+    with open(FILENAME, "r", encoding="utf-8") as file:
+        lista = file.readlines()
         return [line.replace("\n","") for line in lista]
-    
+
 def carregar_listbox():
     itemList = carregar_items()
     for item in itemList:
@@ -18,12 +21,27 @@ def limpar_campos():
     cpf.delete(0, END)
     idade.delete(0, END)
 
+def salvar_lista(lista):
+    with open(FILENAME, "w", encoding="utf-8") as arquivo:
+        for i in lista:
+            arquivo.write(i + "\n")
+
+        limpar_campos()
+
 def onSelect(event):
-    item = listbox.get(int(listbox.curselection()[0])).split(" - ")
-    nomeVar.set(item[0])
-    enderecoVar.set(item[1])
-    cpfVar.set(item[2])
-    idadeVar.set(item[3])
+    global is_updating, selected_index
+    try:
+        selected_index = listbox.curselection()[0]
+        item = listbox.get(selected_index).split(" - ")
+
+        nomeVar.set(item[0])
+        enderecoVar.set(item[1])
+        cpfVar.set(item[2])
+        idadeVar.set(item[3])
+
+        is_updating = True
+    except IndexError:
+        pass
 
 def atualizar_usuario():
     lista = carregar_items()
@@ -33,62 +51,43 @@ def atualizar_usuario():
     idade_input = idade.get().strip()
     cpf_input = cpf.get().strip()
 
-    item = f"{nome_input} - {endereco_input} - {cpf_input} - {idade_input}"
-    item_listbox = listbox.get(listbox.curselection()[0])
+    new_item = f"{nome_input} - {endereco_input} - {cpf_input} - {idade_input}"
 
-    if(item != item_listbox):
-        if(item in lista):
-            # voltar aqui
-            res = messagebox.showwarning("Valores duplicados não são aceitos!", f"'{item}' já existe na lista!")
-            if(res):
-                lista.index(item)
-            return
+    if(new_item in lista):
+        messagebox.showwarning("Valores duplicados não são aceitos!", f"'{new_item}' já existe na lista!")
+
+    lista[selected_index] = new_item
+    listbox.delete(selected_index)
+    listbox.insert(selected_index, new_item)
+    salvar_lista(lista)
+
+def cadastrar_usuario():
+    nome_input = nome.get().strip()
+    endereco_input = endereco.get().strip()
+    idade_input = idade.get().strip()
+    cpf_input = cpf.get().strip()
         
-        lista[lista.index(item_listbox)] = item
+    item = f"{nome_input} - {endereco_input} - {cpf_input} - {idade_input}"
 
-        with open(FILENAME, "r+", encoding="utf-8") as arquivo:
-            # setando o mouse no inicio e apagando todo o conteudo do arquivo
-            arquivo.seek(0)
-            arquivo.truncate(0)
+    if(nome_input == "" or endereco_input == "" or idade_input == 0 or cpf_input == ""):
+        messagebox.showwarning("Erro nos campos", "Os campos não podem estar vazios!")
+        return
+        
+    lista = carregar_items()
+    if(item in lista):
+        messagebox.showwarning("Valores duplicados não são aceitos!", f"'{item}' já existe na lista!")
 
-            # escrevendo a nova lista, ja tendo removido o item deletado
-            for i in lista:
-                arquivo.write(i + "\n")
+    with open(FILENAME,"a", encoding="utf-8") as arquivo:
+        arquivo.write(item + "\n")
 
-            # pegando o index do item a ser deletado na listbox e removendo-o
-            idx = listbox.get(0, END).index(item_listbox)
-            listbox.delete(idx)
-            listbox.insert(idx, item)
-            limpar_campos()
+    listbox.insert(END, item)
+    limpar_campos()
 
-def salvar_usuario():
-    if(len(listbox.curselection()) > 0):
+def save_button():
+    if(is_updating):
         atualizar_usuario()
     else:
-        nome_input = nome.get().strip()
-        endereco_input = endereco.get().strip()
-        idade_input = idade.get().strip()
-        cpf_input = cpf.get().strip()
-        
-        item = f"{nome_input} - {endereco_input} - {cpf_input} - {idade_input}"
-
-        if(item.strip("-") == ""):
-            messagebox.showwarning("Erro nos campos", "Os campos não podem estar vazios!")
-            return
-        
-        lista = carregar_items()
-        if(item in lista):
-            # voltar aqui
-            res = messagebox.showwarning("Valores duplicados não são aceitos!", f"'{item}' já existe na lista! Você está tentando atualizar o item?")
-            if(res):
-                lista.index(item)
-            return
-
-        with open(FILENAME,"a", encoding="utf-8") as arquivo:
-            arquivo.write(item + "\n")
-
-        listbox.insert(END, item)
-        limpar_campos()
+        cadastrar_usuario()
     
 def deletar_usuario():
     try:
@@ -141,7 +140,7 @@ listbox = Listbox(window, width=40,height=10, selectmode=SINGLE)
 
 listbox.bind("<<ListboxSelect>>", onSelect)
 
-salvar = Button(window,text="Salvar", command=salvar_usuario)
+salvar = Button(window,text="Salvar", command=save_button)
 deletar = Button(window, text="Deletar", command=deletar_usuario)
 
 # configuracao do layout com grid
@@ -156,7 +155,7 @@ idade.grid(row=3,column=1)
 salvar.grid(row=4, column=0)
 deletar.grid(row=4, column=1)
 listboxLabel.grid(row=5)
-listbox.grid(row=6 ,column=0,columnspan=2)
+listbox.grid(row=6 ,column=0,columnspan=2, padx=(10,0))
 
 carregar_listbox()
 window.mainloop()
