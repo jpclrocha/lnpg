a82f1cf21fbc7bda5f09dd950b61b86216ad5ad1
JOÃO PEDRO DE CASTRO LARANJEIRA ROCHA <2023007098@MACEIO.IFAL>
2024-09-26 21:46:29 -0300

    Second question

---
+++ working-with-c/lista-1/q2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char const *args[]){
+    int v1,v2,v3,v4;
+    printf("Digite um valor inteiro: ");
+    scanf("%d", &v1);
+
+    printf("Digite um valor inteiro: ");
+    scanf("%d", &v2);
+
+    printf("Digite um valor inteiro: ");
+    scanf("%d", &v3);
+
+    printf("Digite um valor inteiro: ");
+    scanf("%d", &v4);
+    printf("Soma dos valores: %d\n", v1 + v2 + v3+ v4);
+}
